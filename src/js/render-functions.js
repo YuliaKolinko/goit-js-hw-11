0a65632008a0f3095b4a59b91c5597b6cc00784d
Yulia <derevyanko.julia@gmail.com>
2025-03-06 12:09:36 +0200

    fix span

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -33,15 +33,15 @@ export default function renderImages(images) {
     <span class="description-name">Likes</span>
     <span class="description-counts">${likes}</span>
   </div>
-  <div class="item-info-container
+  <div class="item-info-container">
     <span class="description-name">Views</span>
     <span class="description-counts">${views}</span>
   </div>
-  <div class="item-info-container
+  <div class="item-info-container">
     <span class="description-name">Comments</span>
     <span class="description-counts">${comments}</span>
   </div>
-  <div class="item-info-container
+  <div class="item-info-container">
     <span class="description-name">Downloads</span>
     <span class="description-counts">${downloads}</span>
   </div>
